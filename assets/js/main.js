404568a8694f9c33f2de19398cfc0fb0299b68e8
AlexanderPistiulha <olexanderfreelance@gmail.com>
2020-07-29 11:36:43 +0300

    added slick slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,41 @@
 document.querySelector('.mobile__icon').addEventListener('click', function () {
     this.classList.toggle('mobile__icon-active');
     document.querySelector('.mobile__body').classList.toggle('mobile__body-active');
-})
+});
+
+
+//  -----------------------------------slider-----------------------------------
+$(document).ready(function () {
+    $('.slider').slick({
+
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1350,
+                settings: {
+                    slidesToShow: 2,
+
+                }
+            },
+            {
+                breakpoint: 860,
+                settings: {
+                    slidesToShow: 2,
+
+                }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1,
+                    arrows: false,
+
+                }
+            }
+
+        ]
+    });
+});
